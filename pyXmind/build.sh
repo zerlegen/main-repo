a5a7d7af2f599715cc45985184a55cd8ca8a4a26
zerlegen <zerlegen@gmail.com>
2013-06-29 12:14:39 -0700

    Updates 6/29/13

--- pyXmind/build.sh
+++ pyXmind/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
 # run xmind-xml.py and build a test xmind workbook
-./xmind-xml.py > build/content.xml
-zip build/build.xmind build/content.xml build/META-INF build/Thumbnails build/meta.xml build/Revisions
+cd build
+unzip build.xmind.original 
+../xmind-xml.py > content.xml
+zip build.xmind content.xml META-INF Thumbnails meta.xml Revisions
+rm -rf *.xml META-INF Thumbnails Revisions
