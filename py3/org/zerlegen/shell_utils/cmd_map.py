52a0b61524a0cb61ba7bacbb091f7c2c80e3f306
zerlegen <zerlegen@gmail.com>
2014-07-19 19:58:12 -0700

    ability to add sys.argv

--- py3/org/zerlegen/shell_utils/cmd_map.py
+++ py3/org/zerlegen/shell_utils/cmd_map.py
@@ -1,7 +1,9 @@
 #!python3
 
 class command_mappings:
-  
+
+################################################################################ 
+
   _aliases = []
   #_tag_lists
   _cmd_strs = []
@@ -10,8 +12,8 @@ class command_mappings:
     self._cmd_strs.append(cmd)
     #self._tag_lists.append(tag_list)
     self._aliases.append(alias)
-    print(self._cmd_strs)
-    print(self._aliases)
+    #print(self._cmd_strs)
+    #print(self._aliases)
 
   def generate_shortcuts(self): 
     for i in range(len(self._cmd_strs)):
@@ -21,8 +23,31 @@ class command_mappings:
         out_file.write("#!python3\n")
         out_file.write("\n")
         out_file.write("import cmd_utils\n")
+        out_file.write("import sys\n")
         out_file.write("\n")
         out_file.write("eval(\"" + "cmd_utils." + cmd + "\")") 
+
+################################################################################
+
+def add_bash_mapping(line, map_obj):
+  tokens = line.split()
+  alias = tokens[1]
+  in_list = tokens[2:]
+  in_str = "print_command_output(["
+  for i in range(len(in_list)):
+    if (in_list[i]).startswith("sys.argv"): 
+      in_str += in_list[i] 
+    else:
+      in_str += "\'" + in_list[i] + "\'"
+    if (i + 1) < len(in_list):
+      in_str += ", "
+  in_str += "])"
+  print(in_str)
+  map_obj.add_mapping(in_str, alias, [])
+ 
+
+
+################################################################################
 
 if __name__ == "__main__":
 
@@ -32,18 +57,7 @@ if __name__ == "__main__":
   with open("commands.txt", "r") as in_file:
     for line in in_file.readlines():
       if line.startswith("bash"):
-        tokens = line.split()
-        alias = tokens[1]
-        in_list = tokens[2:]
-        in_str = "print_command_output(["
-        for i in range(len(in_list)):
-          in_str += "\'" + in_list[i] + "\'"
-          if (i + 1) < len(in_list):
-            in_str += ", "
-        in_str += "])"
-        #print(in_str)
-        map1.add_mapping(in_str, alias, [])
-        
+        add_bash_mapping(line, map1) 
                          
 
   map1.generate_shortcuts()
