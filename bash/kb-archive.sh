5cbb72b931b18ccb230e76bf26bc2f19fad66d9b
epom <epomeric@gmail.com>
2016-12-24 19:42:27 -0800

    use softlink for vimwiki, use Twofish

--- bash/kb-archive.sh
+++ bash/kb-archive.sh
@@ -3,8 +3,8 @@ cd ~/
 TIMESTAMP=`date +%Y-%m-%d-%H-%M`
 STAGING_DIR="archive-staging-$TIMESTAMP"
 mkdir $STAGING_DIR
-tar -cvf $STAGING_DIR/archive-$TIMESTAMP.tar vimwiki
+tar -cvf $STAGING_DIR/archive-$TIMESTAMP.tar archived
 gzip -cf $STAGING_DIR/archive-$TIMESTAMP.tar > $STAGING_DIR/archive-$TIMESTAMP.tar.gz
-gpg --symmetric $STAGING_DIR/archive-$TIMESTAMP.tar.gz
+gpg --symmetric --cipher-algo TWOFISH $STAGING_DIR/archive-$TIMESTAMP.tar.gz
 cp $STAGING_DIR/archive-$TIMESTAMP.tar.gz.gpg ~/Dropbox/
 rm -rf $STAGING_DIR
